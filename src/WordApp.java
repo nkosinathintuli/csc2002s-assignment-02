f333ab11f8b8f356e4c6b99b0e04cb3902597112
Nkosinathi Ntuli <51000737+nkosinathintuli@users.noreply.github.com>
2021-09-10 18:20:10 +0200

    fiixing error in wordapp

--- src/WordApp.java
+++ src/WordApp.java
@@ -17,14 +17,15 @@ import java.util.concurrent.*;
 
 public class WordApp {
 //shared variables
-        static String text="";
+	static String text="";
 	static int noWords=4;
 	static int totalWords;
 
    	static int frameX=1000;
 	static int frameY=600;
 	static int yLimit=480;
-        static int num=0,con=0;
+        static int num=0;
+	static int con=0;
 	static WordDictionary dict = new WordDictionary(); //use default dictionary, to read from file eventually
 
 	static WordRecord[] words;
@@ -32,151 +33,117 @@ public class WordApp {
 	public static Score score = new Score();
 
 	static WordPanel w;
-	
-	
-	
-	public static void setupGUI(int frameX,int frameY,int yLimit) {
 
-            
-            
+	public static void setupGUI(int frameX,int frameY,int yLimit) {
 		// Frame init and dimensions
-    	JFrame frame = new JFrame("WordGame"); 
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.setSize(frameX, frameY);
-    	
-      	JPanel g = new JPanel();
-        g.setLayout(new BoxLayout(g, BoxLayout.PAGE_AXIS)); 
-      	g.setSize(frameX,frameY);
- 
-    	
+		JFrame frame = new JFrame("WordGame"); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(frameX, frameY);
+
+		JPanel g = new JPanel();
+		g.setLayout(new BoxLayout(g, BoxLayout.PAGE_AXIS)); 
+		g.setSize(frameX,frameY);
 		w = new WordPanel(words,yLimit);
 		w.setSize(frameX,yLimit+100);
-	    g.add(w);
-	    
-	    
-	    JPanel txt = new JPanel();
-	    txt.setLayout(new BoxLayout(txt, BoxLayout.LINE_AXIS)); 
-	    JLabel caught =new JLabel("Caught:" + w.score.getCaught() + "    ");
-	    JLabel missed =new JLabel("Missed:" + w.score.getMissed()+ "    ");
-	    JLabel scr =new JLabel("Score:" + w.score.getScore()+ "    ");    
-	   txt.add(caught);
-	    txt.add(missed);
-	    txt.add(scr);
-             
-	    //[snip]
-  
-	    final JTextField textEntry = new JTextField("",20);
-	   textEntry.addActionListener(new ActionListener()
-	    {
-	      public void actionPerformed(ActionEvent evt) {
-	          text = textEntry.getText();
-                  w.word=text;
-                
-	          //[snip]
-	          textEntry.setText("");
-	          textEntry.requestFocus();
-	      }
-	    });
-	   
-	   txt.add(textEntry);
-	   txt.setMaximumSize( txt.getPreferredSize() );
-	   g.add(txt);
-	    
-	    JPanel b = new JPanel();
-        b.setLayout(new BoxLayout(b, BoxLayout.LINE_AXIS)); 
-	   	JButton startB = new JButton("Start");;
-                           
-                           java.util.Timer time =new java.util.Timer();
-                           TimerTask task;
-                           task = new TimerTask() {
-                           //update the score of the game
-                           @Override
-                           public void run() {
-                           scr.setText("Score:" + w.score.getScore() + "    ");
-                           caught.setText("Caught:" + w.score.getCaught()+ "    ");
-                           missed.setText("Missed:" + w.score.getMissed()+ "    ");
-                         
-                           txt.repaint();
-                          
-                               }
-                                   };
-                
-                
-                
-                
-			// add the listener to the jbutton to handle the "pressed" event
-			startB.addActionListener(new ActionListener()
-		    {
-		      public void actionPerformed(ActionEvent e)
-		      {
-                          if(con==0){
-                          
-                          Thread t =new Thread(w);
-                           w.p=0;
-                           //timer excutes keeps updating the score everytime
-                          if(num==0){time.schedule(task,10,10);
-                          num++;
-                          }
-                           w.done=false;
-                           con++;
-                           t.start();}
-                           
-		    	  //[snip]
-		    	  textEntry.requestFocus();  //return focus to the text entry field
-		      }
-		    });
+		g.add(w);
+
+		JPanel txt = new JPanel();
+		txt.setLayout(new BoxLayout(txt, BoxLayout.LINE_AXIS)); 
+		JLabel caught =new JLabel("Caught:" + w.score.getCaught() + "    ");
+		JLabel missed =new JLabel("Missed:" + w.score.getMissed()+ "    ");
+		JLabel scr =new JLabel("Score:" + w.score.getScore()+ "    ");    
+		txt.add(caught);
+		txt.add(missed);
+		txt.add(scr);
+		//[snip]
+
+		final JTextField textEntry = new JTextField("",20);
+		textEntry.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt) {
+				text = textEntry.getText();
+				//[snip]
+				w.word=text;
+				textEntry.setText("");
+				textEntry.requestFocus();
+			}
+		});
+
+		txt.add(textEntry);
+		txt.setMaximumSize( txt.getPreferredSize() );
+		g.add(txt);
+
+		JPanel b = new JPanel();
+		b.setLayout(new BoxLayout(b, BoxLayout.LINE_AXIS)); 
+		JButton startB = new JButton("Start");;
+
+		java.util.Timer time =new java.util.Timer();
+		TimerTask task;
+		task = new TimerTask() {
+			//update the score of the game
+			@Override
+			public void run() {
+				scr.setText("Score:" + w.score.getScore() + "    ");
+				caught.setText("Caught:" + w.score.getCaught()+ "    ");
+				missed.setText("Missed:" + w.score.getMissed()+ "    ");
+				txt.repaint();
+			}
+		};
+
+		// add the listener to the jbutton to handle the "pressed" event
+		startB.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//[snip]
+				if(con==0){
+					Thread t =new Thread(w);
+					w.p=0;
+					//timer excutes keeps updating the score everytime
+					if(num==0){
+						time.schedule(task,10,10);
+						num++;
+					}
+					w.done=false;
+					con++;
+					t.start();
+				}
+				textEntry.requestFocus();  //return focus to the text entry field
+			}
+		});
 		JButton endB = new JButton("End");;
-			
-				// add the listener to the jbutton to handle the "pressed" event
-				endB.addActionListener(new ActionListener()
-			    {
-			      public void actionPerformed(ActionEvent e)
-			      {
-                                
-			    	// w.p++;
-                                w.done=true;
-                                con=0;
-              
-			      }
-			    });
-		 
-                JButton quit = new JButton("Quit");
-                          quit.addActionListener(new ActionListener()
-                                  {
-			      public void actionPerformed(ActionEvent e)
-			      {
-			    	 
-                                  
-                                  
-			      }
-			    });
-                              
+
+		// add the listener to the jbutton to handle the "pressed" event
+		endB.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				// w.p++;
+				w.done=true;
+				con=0;
+			}
+		});
+
+		JButton quit = new JButton("Quit");
+		quit.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+
+			}
+		});
+
 		b.add(startB);
 		b.add(endB);
 		b.add(quit);
 		g.add(b);
-    	
-      	frame.setLocationRelativeTo(null);  // Center window on screen.
-      	frame.add(g); //add contents to window
-        frame.setContentPane(g);     
-       	//frame.pack();  // don't do this - packs it into small space
-        frame.setVisible(true);
 
-		
+		frame.setLocationRelativeTo(null);  // Center window on screen.
+		frame.add(g); //add contents to window
+		frame.setContentPane(g);     
+		//frame.pack();  // don't do this - packs it into small space
+		frame.setVisible(true);	
 	}
 
-        
-        
-
-        
-	
-public static String[] getDictFromFile(String filename) {
+	public static String[] getDictFromFile(String filename) {
 		String [] dictStr = null;
 		try {
 			Scanner dictReader = new Scanner(new FileInputStream(filename));
 			int dictLength = dictReader.nextInt();
 			//System.out.println("read '" + dictLength+"'");
-
 			dictStr=new String[dictLength];
 			for (int i=0;i<dictLength;i++) {
 				dictStr[i]=new String(dictReader.next());
@@ -184,45 +151,43 @@ public static String[] getDictFromFile(String filename) {
 			}
 			dictReader.close();
 		} catch (IOException e) {
-	        System.err.println("Problem reading file " + filename + " default dictionary will be used");
-	    }
+			System.err.println("Problem reading file " + filename + " default dictionary will be used");
+		}
 		return dictStr;
-
 	}
 
 	public static void main(String[] args) {
-    	 Scanner sc = new Scanner(System.in);
-         
+		Scanner sc = new Scanner(System.in);
 		//deal with command line arguments
 		totalWords=Integer.parseInt(args[0]);  //total words to fall
-               // totalWords=sc.nextInt();
-	  noWords=Integer.parseInt(args[1]);
-               // noWords=sc.nextInt();
-                
-                // total words falling at any point
+		// totalWords=sc.nextInt();
+		noWords=Integer.parseInt(args[1]);
+		// noWords=sc.nextInt();
+
+		// total words falling at any point
 		assert(totalWords>=noWords); // this could be done more neatly
 		String[] tmpDict=getDictFromFile(args[2]); //file of words
-		
-                if (tmpDict!=null)
+
+		if (tmpDict!=null){
 			dict= new WordDictionary(tmpDict);
-		
+		}
+
 		WordRecord.dict=dict; //set the class dictionary for the words.
-		
+
 		words = new WordRecord[noWords];  //shared array of current words
-		
+
 		//[snip]
-		
+
 		setupGUI(frameX, frameY, yLimit);  
-    	//Start WordPanel thread - for redrawing animation
+		//Start WordPanel thread - for redrawing animation
 
 		int x_inc=(int)frameX/noWords;
-	  	//initialize shared array of current words
+		//initialize shared array of current words
 
 		for (int i=0;i<noWords;i++) {
 			words[i]=new WordRecord(dict.getNewWord(),i*x_inc,yLimit);
-                       
 		}
-               w.total=totalWords;
+		w.total=totalWords;
 	}
 
 }

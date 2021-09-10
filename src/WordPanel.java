142794b8eb579dea49460eb44f615c643adc1670
Nkosinathi Ntuli <51000737+nkosinathintuli@users.noreply.github.com>
2021-09-10 12:01:25 +0200

    Update WordPanel.java

--- src/WordPanel.java
+++ src/WordPanel.java
@@ -11,41 +11,49 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class WordPanel extends JPanel implements Runnable {
-		public static volatile boolean done;
-		private WordRecord[] words;
-		private int noWords;
-		private int maxY;
+	public static volatile boolean done;
+	private WordRecord[] words;
+	private int noWords;
+	private int maxY;
+	public static  Score score= new Score();
+	public int p=0;
+	public String word="";
+	public int total=0;
 
-		
-		public void paintComponent(Graphics g) {
-		    int width = getWidth();
-		    int height = getHeight();
-		    g.clearRect(0,0,width,height);
-		    g.setColor(Color.red);
-		    g.fillRect(0,maxY-10,width,height);
+	public void paintComponent(Graphics g) {
+		int width = getWidth();
+		int height = getHeight();
+		g.clearRect(0,0,width,height);
+		g.setColor(Color.red);
+		g.fillRect(0,maxY-10,width,height);
 
-		    g.setColor(Color.black);
-		    g.setFont(new Font("Helvetica", Font.PLAIN, 26));
-		   //draw the words
-		   //animation must be added 
-		    for (int i=0;i<noWords;i++){	    	
-		    	//g.drawString(words[i].getWord(),words[i].getX(),words[i].getY());	
-		    	g.drawString(words[i].getWord(),words[i].getX(),words[i].getY()+20);  //y-offset for skeleton so that you can see the words	
-		    }
-		   
-		  }
-		
-		WordPanel(WordRecord[] words, int maxY) {
-			this.words=words; //will this work?
-			noWords = words.length;
-			done=false;
-			this.maxY=maxY;		
-		}
-		
-		public void run() {
-			//add in code to animate this
+		g.setColor(Color.black);
+		g.setFont(new Font("Helvetica", Font.PLAIN, 26));
+		//draw the words
+		//animation must be added 
+		for (int i=0;i<noWords;i++){	    	
+			//g.drawString(words[i].getWord(),words[i].getX(),words[i].getY());	
+			g.drawString(words[i].getWord(),words[i].getX(),words[i].getY()+20);  //y-offset for skeleton so that you can see the words	
 		}
 
 	}
+
+	WordPanel(WordRecord[] words, int maxY) {
+		this.words=words; //will this work?
+		noWords = words.length;
+		done=false;
+		this.maxY=maxY;		
+	}
+
+	public void run() {
+		//add in code to animate this
+		for (int i=0; i<noWords;i++){
+			execute x = new execute(i);
+			x.start();
+		}
+	}
+
+	
+}
 
 
